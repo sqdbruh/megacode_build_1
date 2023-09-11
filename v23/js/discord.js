772d1efabac5e0b060831be7b02d096550397e95
Grigory Pavlichenko <HardToHandle@yandex.ru>
2023-09-11 13:27:31 +0300

    v23

---
+++ v23/js/discord.js
@@ -0,0 +1,43 @@
+const channel = new BroadcastChannel('app-data');
+var eventListeners = [];
+
+function signWithDiscord(redirectUri, callback){
+	removeEventListener();
+	
+	const eventListener = event => {
+		channel.removeEventListener('message', eventListener); 
+		callback(event.data);
+    };
+	
+	this.eventListeners.push(eventListener); 
+	channel.addEventListener ('message', eventListener);
+
+	var uri = "https://discord.com/api/oauth2/authorize?client_id=1092513317169414235&redirect_uri=" + redirectUri + "&response_type=code&scope=identify%20guilds%20email";
+	var disc_window = window.open(uri);
+}
+
+function authWithDiscord(){
+	removeEventListener();
+	
+	const eventListener = event => {
+		channel.removeEventListener('message', eventListener); 
+    };
+	
+	this.eventListeners.push(eventListener); 
+	channel.addEventListener ('message', eventListener);
+	
+	var port = window.location.port;
+	var port_string = port ? "%3A" + port : "";
+	var redirect_uri = "http%3A%2F%2F" + window.location.hostname + port_string  +"%2Fdiscord_wait.html";
+	var uri = "https://discord.com/api/oauth2/authorize?client_id=1092513317169414235&redirect_uri=" + redirect_uri + "&response_type=code&scope=identify%20guilds%20email";
+	var disc_window = window.open(uri);
+}
+
+function removeEventListener() {
+  // Iterate through all stored event listeners to remove them
+  this.eventListeners.forEach((listener) => {
+      channel.removeEventListener('message', listener); 
+  });
+  // Do not forget to clear listeners array
+  this.eventListeners = [];
+}
