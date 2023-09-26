197b0574dcbddc642c8ad52a5d6ab29f77c9ccb5
Grigory Pavlichenko <HardToHandle@yandex.ru>
2023-09-26 14:50:31 +0300

    v28

---
+++ v28/js/analytics_constants.js
@@ -0,0 +1,10 @@
+const G_TAG = "G-SGFC31VKKL";
+
+const HTML_No_Mobile_Version_Screen_Showed = "HTML-No_Mobile_Version-Screen-Showed";
+const HTML_Detect_Incorrect_Browser = "HTML-Detect_Incorrect_Browser";
+const Unity_WebGL_Hello_world = "Unity_WebGL-Hello_world";
+const Splash_Screen_Loading_Finished = "Splash-Screen_Loading_Finished";
+const Client_MaintenanceBreak_TryAgain_Click = "Client_MaintenanceBreak_TryAgain_Click";
+const Splash_Screen_Showed = "Splash-Screen-Showed";
+const Splash_Screen_Loading_Started = "Splash-Screen-Loading-Started";
+const Hello_World = "Hello_World";
